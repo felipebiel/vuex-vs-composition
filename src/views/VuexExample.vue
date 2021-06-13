4e107041ea1b92c7e44723ea2a78965f5ac8fc4b
Felipe Biel <felipe.biel.souza@gmail.com>
2021-06-13 10:00:27 -0300

    feat: Exemplo básico do vuex

--- src/views/VuexExample.vue
+++ src/views/VuexExample.vue
@@ -1,11 +1,26 @@
 <template>
-    <div>Exemplo de contador com Vuex</div>
+    <div>
+        <h2>Exemplo de contador com Vuex {{ count }} {{ count1 }}</h2>
+        <button @click="increment1">incrementar 1</button>
+        <button @click="increment1">incrementar 2</button>
+    </div>
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
+
 export default {
-    setup() {
-        return {};
+    methods: {
+        ...mapActions(["increment"]),
+        increment1() {
+            this.$store.dispatch("increment");
+        }
+    },
+    computed: {
+        ...mapState(["count"]),
+        count1() {
+            return this.$store.state.count;
+        }
     }
 };
 </script>
